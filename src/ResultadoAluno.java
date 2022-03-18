03a606342fedf03fc1fcb13eff354a8824daf0d8
Thiago de Jesus Barbosa <rm79777@estudante.fieb.edu.br>
2022-03-18 12:16:52 -0300

    Primeiro Commit

---
+++ src/ResultadoAluno.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class ResultadoAluno {
+
+	public static void main(String[] args) {
+		
+		double media = Double.parseDouble(JOptionPane
+				.showInputDialog(null, "Média:"))
+		
+		
+		if(media >=6.0) {
+			JOptionPane.showMessageDialog(null,
+					"aprovado!");
+		} else if (media < 6.0) {
+			JOptionPane.showMessageDialog(null,
+					"Reprovado");
+		}
+		
+
+	}
+
+}
